90ea29a62e35f6591e82905ef6b44ca5ae9189ab
Arif2ankhi <arif2ankhi@gmail.com>
2026-09-11 00:04:24 +0100

    Project set up and nav bar completed

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Banner from "./components/Banner"
+import Nav from "./components/Nav"
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Nav></Nav>
+      <Banner></Banner>
+     {/* <h2 className="text-5xl text-green-500">Dev Stack</h2> */}
+     {/* <button className='btn btn-error'>Button</button> */}
+          
+    </>
+  )
+}
+
+export default App
